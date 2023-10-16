eaace987b7963fe9ddcc9fd96d321558421ffa3f
cloudwipe <roma.isaev.2000@gmail.com>
2023-10-16 21:06:06 +0300

    Update move-string.js

--- js/move-string.js
+++ js/move-string.js
@@ -1,5 +1,5 @@
 let content =
-  '<div class="move-container"><p style="margin-right: 5px" class="move-string">Курс для лікарів</p><img src="../images/move-string-plus.svg" alt="plus" height="17" width="17" /></div>';
+  '<div class="move-container"><p style="margin-right: 5px" class="move-string">Курс для лікарів</p><img src="./images/move-string-plus.svg" alt="plus" height="17" width="17" /></div>';
 
 const mySuperMarquee = new SuperMarquee(
   document.getElementById("supermarquee"),
